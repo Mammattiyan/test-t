0071af6023de0060432840d9847619478f57e702
monish <monish3636@gmail.com>
2017-07-08 19:13:38 +0530

    message listing

--- app/Modules/Message/Http/Controllers/Message.php
+++ app/Modules/Message/Http/Controllers/Message.php
@@ -5,6 +5,10 @@ namespace App\Modules\Message\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Modules\Message\Models\Messages;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Collection;
 
 class Message extends Controller {
     /*
@@ -16,7 +20,27 @@ class Message extends Controller {
      */
 
     public function indexAction() {
-         return view('message::index');
+
+        $useId = Auth::User()->id;
+//
+        $receiver = Messages::select('messages.*', DB::raw('(SELECT name FROM users wHERE users.id =messages.sender) AS messager_name'),DB::raw('(SELECT profileimage FROM users wHERE users.id =messages.sender) AS profileimage'))
+                        ->join('users', 'users.id', 'messages.receiver')
+                        ->where('messages.receiver', $useId)
+                        ->orderBY('messages.id', 'desc')
+                        ->get()->toArray();
+        $sender = Messages::select('messages.*',DB::raw('(SELECT name FROM users wHERE users.id =messages.receiver) AS messager_name'),DB::raw('(SELECT profileimage FROM users wHERE users.id =messages.receiver) AS profileimage'))
+                        ->join('users', 'users.id', 'messages.sender')
+                        ->where('messages.sender', $useId)
+                        ->orderBY('messages.id', 'desc')
+                        ->get()->toArray();
+        
+      
+      
+       
+        $allMessage = collect(array_merge($receiver, $sender));
+        $keyed = $allMessage->sortByDesc('id');
+        $final = $keyed->all();
+        return view('message::index')->with(['messages'=>$final]);
     }
 
 }
