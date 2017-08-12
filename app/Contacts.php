763b46bec594c3a0453cfacb945353ca5fd0d37b
Your Name <you@example.com>
2017-08-12 19:22:26 +0530

    message

---
+++ app/Contacts.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Contacts extends Model {
+
+    protected $guarded = [];
+    protected $table = 'contacts';
+
+}
