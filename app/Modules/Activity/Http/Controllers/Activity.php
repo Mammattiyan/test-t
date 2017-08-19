6660898068b0b64484c167dee34f8c943122e53f
Your Name <you@example.com>
2017-08-19 16:51:31 +0530

    message

--- app/Modules/Activity/Http/Controllers/Activity.php
+++ app/Modules/Activity/Http/Controllers/Activity.php
@@ -5,6 +5,13 @@ namespace App\Modules\Activity\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Modules\Hangout\Models\Hangouts;
+use App\Modules\Message\Models\Dine;
+use App\Modules\Message\Models\Messages;
+use App\Modules\Hangout\Models\Recent_activity;
+use Illuminate\Support\Facades\DB;
+use App\Modules\Core\Http\Controllers\Core;
 
 class Activity extends Controller {
     /*
@@ -16,7 +23,22 @@ class Activity extends Controller {
      */
 
     public function indexAction() {
-        return view('activity::index');
+        $userId = Auth::User()->id;
+        $data = [];
+        $data['hangoutCount'] = Hangouts::select('sender_id')->where('hangouts.receiver_id', $userId)->count();
+        $data['dineCount'] = Dine::select('sender_id')->where('dines.receiver_id', $userId)->count();
+        $data['messageCount'] = Messages::select('sender')->where('messages.receiver', $userId)->count();
+        $data['notification'] = $data['hangoutCount'] + $data['dineCount'] + $data['messageCount'];
+        $data['recentActivityCount'] = Recent_activity::select('sender')->where('recent_activities.user_id', $userId)->count();
+        $recentData = Recent_activity::select('recent_activities.*', 'users.name AS user_name', 'users.profileimage AS user_profile_pic', DB::raw('(SELECT name FROM users WHERE users.id= recent_activities.receiver_id) AS receiver_name'), DB::raw('(SELECT profileimage FROM users WHERE users.id= recent_activities.receiver_id) AS receiver_profile_pic'))
+                ->join('users', 'users.id', 'recent_activities.user_id')
+                ->where('recent_activities.user_id', $userId)
+                ->orWhere('recent_activities.receiver_id', $userId)
+                ->take(5)
+                ->get()
+                ->toArray();
+
+        return view('activity::index')->with(['data' => $data, 'recentData' => $recentData, 'userId' => $userId, 'token' => Core::encodeIdAction($userId)]);
     }
 
 }
--- app/Modules/Activity/Resources/Views/index.blade.php
+++ app/Modules/Activity/Resources/Views/index.blade.php
@@ -5,30 +5,29 @@
 @section('content')
 <div class="flex-item updates-block">
     <div class="box">
-       @include('search::search_form')
-       
-        <hr>
+        @include('search::search_form')
+        <hr>        
         <div class="pad">
             <div class="thick-text">Notifications</div>
             <div class="notifications-wrap">
                 <div class="notification">
-                    <div class="noti-count">37</div>
+                    <div class="noti-count">{{$data['messageCount']}}</div>
                     <div class="noti-name">Message</div>
                 </div>
                 <div class="notification">
-                    <div class="noti-count">345</div>
+                    <div class="noti-count">{{$data['notification']}}</div>
                     <div class="noti-name">Notification</div>
                 </div>
                 <div class="notification">
-                    <div class="noti-count">12</div>
+                    <div class="noti-count">{{$data['dineCount']}}</div>
                     <div class="noti-name">Dining</div>
                 </div>
                 <div class="notification">
-                    <div class="noti-count">127</div>
+                    <div class="noti-count">{{$data['hangoutCount']}}</div>
                     <div class="noti-name">Hangout</div>
                 </div>
                 <div class="notification">
-                    <div class="noti-count">15</div>
+                    <div class="noti-count">{{$data['recentActivityCount']}}</div>
                     <div class="noti-name">Activity</div>
                 </div>
             </div>
@@ -36,45 +35,49 @@
     </div>
     <div class="box pad">
         <div class="thick-text">Recent Activity</div>
+        @if(count($recentData) > 0)
+        @foreach($recentData as $key =>$value)
+        @if($value['user_id']==$userId)
         <div class="activity">
-            <img src="../assets/user2.jpg" class="user-icon">
+            <img src="{{ asset($value['receiver_profile_pic'])}}" class="user-icon">
             <div class="activity-text">
-                <div class="bold-text">Shay Laren</div>
-                <div class="activity-details">has sent a date request</div>
-                <div class="timestamp">11:32 PM</div>
+                <div class="bold-text">{{$value['receiver_name']}}</div>
+                <div class="activity-details">{{$value['display_message']}}</div>
+                <div class="timestamp">{{$value['created_at']}}</div>
             </div>
         </div>
-        <div class="activity">
-            <img src="../assets/user3.jpg" class="user-icon">
+        
+        @else
+         <div class="activity">
+            <img src="{{ asset($value['receiver_profile_pic'])}}" class="user-icon">
             <div class="activity-text">
-                <div class="bold-text">Shay Laren</div>
-                <div class="activity-details">has sent a hangout request</div>
-                <div class="timestamp">10:24 PM</div>
+                <div class="bold-text">{{$value['receiver_name']}}</div>
+                <div class="activity-details">You receive {{$value['module_name']}} request</div>
+                <div class="timestamp">{{$value['created_at']}}</div>
             </div>
-        </div>
+        </div>      
+        @endif
+        
+        @endforeach
+        @else
         <div class="activity">
-            <img src="../assets/user4.jpg" class="user-icon">
+          
             <div class="activity-text">
-                <div class="activity-details">You have sent a hangout request to</div>
-                <div class="bold-text">Veronica Zamenova</div>
-                <div class="timestamp">08:32 AM</div>
+                <div class="bold-text">No Recent Activity Found</div>
+               
             </div>
-        </div>
-        <div class="activity">
-            <img src="../assets/user2.jpg" class="user-icon">
-            <div class="activity-text">
-                <div class="bold-text">Shay Laren</div>
-                <div class="activity-details">has sent a date request</div>
-                <div class="timestamp">11:14 PM</div>
-            </div>
-        </div>
-        <div class="activity">
-            <img src="../assets/user1.jpg" class="user-icon">
-            <div class="activity-text">
-                <div class="activity-details">You updated your profile data</div>
-                <div class="timestamp">02:45 PM</div>
-            </div>
-        </div>
+        </div> 
+        
+        @endif
     </div>
 </div>
+
+<div class="hide" data-modal-contents-wrap>
+            <div data-modal-content="trusted">
+                <div data-modal-heading>Trusted</div>
+                <div data-modal-body>
+                   <div class="thick-text"></div>Itweetup verifies users credibility based on the documents user/member provide us and its matched and checked with external document verification service providers and users on the itweetup; who has CV highlighted on their profiles are the members who have verified their Citizenship with itweetup and this makes them more authentic/true members. <br><br>
+                </div>
+            </div>
+        </div>
 @endsection
